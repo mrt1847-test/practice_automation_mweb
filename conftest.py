55349adc92a54cbfcffe0170c2911cc070141a9c
mrt1847 <122250753+mrt1847@users.noreply.github.com>
2025-01-17 16:12:57 +0900

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,12 +9,14 @@ from appium import webdriver
 import psutil
 import json
 import socket
+import getpass
 
 with open('config.json', 'r', encoding='utf-8') as config_file:
     config = json.load(config_file)
 
 @pytest.fixture
 def driver():
+    username = getpass.getuser()
     # 디바이스 및 앱 정보 설정pip
     os_version = platform.platform()
     mobile_emulation = {
@@ -26,10 +28,11 @@ def driver():
         chrome_options.add_argument("--start-maximized")
         driver = selenium.webdriver.Chrome(options=chrome_options, executable_path="c:/webdriver/chromedriver.exe")
     elif 'mac' in os_version:
-        opts = selenium.webdriver.ChromeOptions()
-        opts.add_argument("--start-maximized")
+        chrome_options = selenium.webdriver.ChromeOptions()
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
         driver = selenium.webdriver.Chrome(
-            options=opts, executable_path="/Users/" + username + "/webdriver/chromedriver")
+            options=chrome_options, executable_path="/Users/" + username + "/webdriver/chromedriver")
     yield driver
     # 테스트 종료 후 Appium 서버와 연결 종료
     driver.quit()
